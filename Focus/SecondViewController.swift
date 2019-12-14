0c2e5d929bfba941d3b410dab40956d3bfa2734c
Shahan Neda <shahan.neda@gmail.com>
2019-12-13 18:42:57 -0800

    Initial Commit

---
+++ Focus/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Focus
+//
+//  Created by Shahan Nedadahandeh on 2019-12-13.
+//  Copyright © 2019 Shahan Nedadahandeh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
