0ede2d4154b4ef6fe1c6b20f21032dab1ec98bdd
Shahan Neda <shahan.neda@gmail.com>
2019-12-15 14:56:27 -0800

    got basic timing, along with start and stop buttons

--- Focus/FirstViewController.swift
+++ Focus/FirstViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    private var startTime = Date();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startButton.addTarget(self, action: #selector(self.startButtonClicked(_:)), for: .touchDown);
+        stopButton.addTarget(self, action: #selector(self.stopButtonClicked(_:)), for: .touchDown);
     }
-
-
+    
+    @objc func startButtonClicked(_  sender:  UIButton){
+        print("Start button clicked");
+        print(startTime);
+    }
+    
+    @objc func stopButtonClicked(_ sender: UIButton){
+        print("Stop button clicked");
+        let timeNow = Date();
+        let timePassed = Int(timeNow.timeIntervalSince(startTime));
+        
+        print(timePassed);
+    }
+    
+//    @objc func updateUI(){
+//        print
+//    }
+    
+    
+    
 }
 
